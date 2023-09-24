047b66f9266ddea5c7de62b1d3da74d8e4c1c16a
Diegoh-dev <diegoh.developer@gmail.com>
2023-09-24 18:32:08 -0300

    feat: inicinado perfil do portfolio.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+'use client'
+import './globals.css';
+import Image from "next/image";
+import imageCapa from '../../public/assets/capa.png'
 
 export default function Home() {
   return (
-    <main>
-   <p>portfolio</p>
+    <main className='max-w-full h-screen relative border border-l-blue'>
+      <section className="max-w-full h-2/6 relative">
+        {/* imagem back */}
+        <Image
+        className='max-w-full h-full'
+        src={imageCapa}
+        // width={200}
+        // height={200}
+        alt="capa perfil"
+        loading="lazy"
+        fill={true}
+        objectFit='cover'
+        />
+        portfolio
+      </section>
     </main>
   )
 }
