37a14941a5f50d686070c997fb0844fe6a371836
Diego ribeiro <Diegoh.developer@gmail.com>
2023-09-25 13:58:34 -0300

    feat: iniciando section de pefil.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,32 @@
-'use client'
-import './globals.css';
-import Image from "next/image";
-import imageCapa from '../../public/assets/capa.png'
+"use client"
+import "./globals.css"
+import Image from "next/image"
+import imageCapa from "../../public/assets/capa.png"
 
 export default function Home() {
   return (
-    <main className='max-w-full h-screen relative border border-l-blue'>
+    <main className="max-w-full h-screen relative">
       <section className="max-w-full h-2/6 relative">
-        {/* imagem back */}
         <Image
-        className='max-w-full h-full'
-        src={imageCapa}
-        // width={200}
-        // height={200}
-        alt="capa perfil"
-        loading="lazy"
-        fill={true}
-        objectFit='cover'
+          className="max-w-full h-full"
+          src={imageCapa}
+          alt="capa perfil"
+          loading="lazy"
+          fill={true}
+          objectFit="cover"
         />
         portfolio
+      </section>
+
+    {/* foto de perfil */}
+      <article>
+
+      </article>
+
+
+      <section>
+        <p>Diego Ribeiro</p>
+        <span>Desenvolvedor Front-End</span>
       </section>
     </main>
   )
