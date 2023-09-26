d8a1b9d3d600d109a7f327ac6ec020a7b3cb1be5
Diegoh-dev <diegoh.developer@gmail.com>
2023-09-25 23:24:19 -0300

    feat: chamando hook de theme e verificação de erro de hitratação.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 "use client"
-import "./globals.css"
-import Image from "next/image"
-import imageCapa from "../../public/assets/capa.png"
+import "./globals.css";
+import Image from "next/image";
+import imageCapa from "../../public/assets/capa.png";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { User2 } from "lucide-react";
+import { IconUser } from "@/components/atoms/iconComponents/iconUser";
 
 export default function Home() {
+  const [mounted,setMounted] = useState(false);
+  const {theme,setTheme} = useTheme();
+  useEffect(()=>{
+    setMounted(true);
+  },[]);
+
+  if(!mounted){
+    return null;
+  }
+
   return (
     <main className="max-w-full h-screen relative">
       <section className="max-w-full h-2/6 relative">
@@ -19,14 +33,21 @@ export default function Home() {
       </section>
 
     {/* foto de perfil */}
-      <article>
-
+      <article className="bg-light w-16 h-16 rounded-full ml-8 my-[-32px] absolute">
+       <div>
+       <IconUser/>
+       </div>
       </article>
 
 
-      <section>
-        <p>Diego Ribeiro</p>
-        <span>Desenvolvedor Front-End</span>
+      <section className="py-10 pr-8 pb-2 pl-8">
+        <p className="text-lg font-petrov font-bold tracking-wider">Diego Ribeiro</p>
+        <span className="text-sm tracking-wide">Desenvolvedor Front-End</span>
+      </section>
+
+
+      <section className="p-8">
+        <p className="font-exo tracking-wider">Desenvolvedor Front-end com experiência em React.js, Next.js, Redux, JavaScript, TypeScript, Styled Components, Material UI e consumo de APIs REST. Apaixonado pelo desenvolvimento de componentes reutilizáveis. Também possui experiência em projetos gerenciados por Metodologias Ágeis. Formado em Sistemas de Informação e cursando Especialização em Front-End na Digital House Brasil.</p>
       </section>
     </main>
   )
