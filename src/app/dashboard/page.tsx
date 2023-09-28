a234e969096c31cc3403ba79031ebd328b3fa86a
Diego ribeiro <Diegoh.developer@gmail.com>
2023-09-28 13:58:14 -0300

    feat:iniciando dashboard.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,24 @@
-export default function Dashboard(){
-    return (
-        <div>teste</div>
-    )
+import { IconUser } from "@/components/atoms/iconComponents/iconUser";
+import Image from "next/image";
+import logoPrincipal from '../../../public/assets/LOGO PRINCIPAL.png';
+import "../globals.css";
+
+export default function Dashboard() {
+  return (
+    <main className="p-3">
+      <header className="flex items-center">
+        <div>
+          <IconUser color="var(--color-purple)" />
+        </div>
+
+        <div className="">
+        <Image
+          src={logoPrincipal}
+          alt="logo principal"
+          loading="lazy"
+        />
+        </div>
+      </header>
+    </main>
+  )
 }
