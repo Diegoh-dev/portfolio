48ae91e6dbb65717ce1bcc4b5d8682f94f8c9076
Diegoh-dev <diegoh.developer@gmail.com>
2023-11-05 14:40:00 -0300

    feat[menu]: chamando componentes e ajustes de estilos.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,34 +6,38 @@ import { HeaderPrincipal } from "@/components/atoms/headerPrincipal";
 import { useContext, useEffect, useState } from "react";
 import { MenuMobile } from "@/components/molecule/components/MenuMobile";
 import { ControllerContext } from "@/context/ControlleContext";
+import { MenuDesktop } from "@/components/molecule/components/MenuDesktop";
 
 export default function Dashboard() {
   const { theme, setTheme } = useTheme();
 
-  const {openMenuMobile,setOpenMenuMobile} = useContext(ControllerContext);
-  
-  const [mounted,setMounted] = useState(false);
+  const { openMenuMobile, setOpenMenuMobile } = useContext(ControllerContext);
 
-  useEffect(()=>{
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
     setMounted(true);
-  },[]);
+  }, []);
 
-  if(!mounted){
+  if (!mounted) {
     return null;
   }
 
   return (
-    <>
-       {openMenuMobile && (<MenuMobile/>)}
-    <main className="p-3 w-full h-full overscroll-auto	overflow-auto">
-      <HeaderPrincipal />
+    <div className="tablet:flex w-screen	h-screen	">
+      <div className="tablet:hidden">{openMenuMobile && <MenuMobile />}</div>
 
-      <section className="mt-4">
-        <MenuTabs />
-      </section>
+      <div className="w-1/3		">
+        <MenuDesktop />
+      </div>
 
+      <main className="p-3 w-full h-full overscroll-auto	overflow-auto tablet:w-2/3	">
+        <HeaderPrincipal />
 
-    </main>
-    </>
+        <section className="mt-4">
+          <MenuTabs />
+        </section>
+      </main>
+    </div>
   );
 }
