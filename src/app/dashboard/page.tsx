8e9f826b7885526eca2990a561edc17433e6ee0b
Diegoh-dev <diegoh.developer@gmail.com>
2023-11-06 23:39:49 -0300

    styele[menu]: largura dos component do dashboard.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -24,7 +24,7 @@ export default function Dashboard() {
   }
 
   return (
-    <div className="tablet:flex w-screen	h-screen	">
+    <div className="tablet:flex tablet:w-2/3 laptop:w-3/4 h-screen	">
       <div className="tablet:hidden">{openMenuMobile && <MenuMobile />}</div>
 
       {/* <div className="hidden tablet:w-1/3	laptop:w-1/4 sm:block">
