562b7bddca0f89252b64fcc00d8b45f56136d218
Diego ribeiro <diego@sistemabiodata.com.br>
2023-11-09 13:51:42 -0300

    colocando lista de projetos na pagina de projetos.

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,7 +1,75 @@
+'use client'
+import { Post } from "@/components/molecule/components/Post/post";
+import { BarChart2, Heart, MessageCircle, Share2 } from "lucide-react";
+
 export default function Projetos(){
     return (
-        <div>
-        projetos diego dev
+        //mt-9 fazer o calc
+        <div className=" overflow-auto flex-col tablet:flex tablet:w-2/3 laptop:w-3/4 h-screen ">
+          <Post.Root>
+            <Post.Header />
+            <Post.PostSection>
+              <Post.SectionHeader />
+              <Post.Content />
+              <Post.PostImage />
+              <Post.FooterRoot>
+                <Post.PostIcon Icon={MessageCircle}>
+                  <Post.Amount amount={150} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={Heart}>
+                  <Post.Amount amount={500} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={BarChart2}>
+                  <Post.Amount amount={1000} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={Share2}></Post.PostIcon>
+              </Post.FooterRoot>
+            </Post.PostSection>
+          </Post.Root>
+
+          <Post.Root>
+            <Post.Header />
+            <Post.PostSection>
+              <Post.SectionHeader />
+              <Post.Content />
+              <Post.PostImage />
+              <Post.FooterRoot>
+                <Post.PostIcon Icon={MessageCircle}>
+                  <Post.Amount amount={150} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={Heart}>
+                  <Post.Amount amount={500} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={BarChart2}>
+                  <Post.Amount amount={1000} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={Share2}>
+                  
+                </Post.PostIcon>
+              </Post.FooterRoot>
+            </Post.PostSection>
+          </Post.Root>
+
+          <Post.Root>
+            <Post.Header />
+            <Post.PostSection>
+              <Post.SectionHeader />
+              <Post.Content />
+              <Post.PostImage />
+              <Post.FooterRoot>
+                <Post.PostIcon Icon={MessageCircle}>
+                  <Post.Amount amount={150} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={Heart}>
+                  <Post.Amount amount={500} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={BarChart2}>
+                  <Post.Amount amount={1000} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={Share2}></Post.PostIcon>
+              </Post.FooterRoot>
+            </Post.PostSection>
+          </Post.Root>
         </div>
     )
 }
