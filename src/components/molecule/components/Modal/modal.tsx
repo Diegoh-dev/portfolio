48906c1b955ab8893560345bb3e7a618385e352e
Diegoh-dev <diegoh.developer@gmail.com>
2023-11-16 21:33:36 -0300

    style[modal]: backgroud da modal de acordo com o tema.

--- src/components/molecule/components/Modal/modal.tsx
+++ src/components/molecule/components/Modal/modal.tsx
@@ -15,7 +15,7 @@ export function Modal({ openModal, setOpenModal }: ModalProps) {
   
       <Dialog.Portal>
         {/* <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" /> */}
-        <Dialog.Content className="data-[state=open]:animate-contentShow dark:bg-dark fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[560px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+        <Dialog.Content className={`${resolvedTheme === 'dark' ? 'bg-dark': resolvedTheme === 'littleDark' ? 'bg-littleDark' : 'bg-light'} data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[560px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none`}>
           <Dialog.Title className="text-mauve12 m-0 text-2xl font-medium">
             Personalizar sua exibição
           </Dialog.Title>
