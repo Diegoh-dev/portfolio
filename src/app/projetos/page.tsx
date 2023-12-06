abaf3b001ea26343c7f76fd078320f10c0f7f493
Diegoh-dev <diegoh.developer@gmail.com>
2023-12-06 19:36:43 -0300

    feat: colocando imagens de projetos em desenvolvimento.

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -6,6 +6,7 @@ import postImage from '../../../public/assets/capa.png';
 import capaVolvo from '../../../public/assets/ProductList-Desktop.png';
 import capaCapsulaDoTempo from '../../../public/assets/Memories.png';
 import capaPortifolio from '../../../public/assets/portifolioNovo.png';
+import capaDesenvolvimento from '../../../public/assets/emDesenvolvimento.png';
 import { FooterComponent } from "@/components/molecule/components/Footer/footer";
 
 export default function Projetos(){
@@ -21,6 +22,7 @@ export default function Projetos(){
     return (
         //mt-9 fazer o calc
         <div className=" overflow-auto w-full flex-col tablet:flex tablet:w-2/3 laptop:w-3/4 sm:h-[calc(100dvh-57px)] h-screen">
+
            <Post.Root>
             <Post.Header />
             <Post.PostSection>
@@ -84,6 +86,85 @@ export default function Projetos(){
               <Post.Content texto="Desafio Volvo: No âmbito deste desafio, explorei minhas habilidades na construção de um projeto inovador totalmente desenvolvido em Next.js 13 e alimentado por Typescript. Minha abordagem para esse desafio envolveu a aplicação rigorosa do conceito de componentização do React, visando eliminar redundâncias de código e promover uma arquitetura eficiente.
                 A estrutura do projeto foi planejada para oferecer páginas dinâmicas que proporcionam uma experiência envolvente, permitindo a visualização detalhada de cada carro e simplificando o processo de compra. Ao enfrentar esse desafio, busquei não apenas atender às expectativas, mas superá-las, demonstrando minha paixão pelo desenvolvimento front-end e minha capacidade de criar soluções que transcendem o comum. Este projeto não é apenas um exemplo de minhas habilidades técnicas, mas também reflete meu compromisso em proporcionar experiências cativantes aos usuários. Clique na imagem para ser redirecionado para o repositório dessa aplicação. 🤗😎"/>
               <Post.PostImage src={capaVolvo} href={"https://github.com/Diegoh-dev/volvo-teste" || ''}/>
+              <Post.FooterRoot>
+                <Post.PostIcon Icon={MessageCircle} onClick={()=> {
+                  toast.warn('Em breve, você poderá comentar nesse post')
+                }}>
+                  <Post.Amount amount={150} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={Heart} onClick={()=> {
+                  toast.warn('Em breve, você poderá curtir esse post')
+                }}>
+                  <Post.Amount amount={500} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={BarChart2} onClick={()=> {
+                  toast.warn('Em breve, total de visualizações')
+                }}>
+                  <Post.Amount amount={1000} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={Share2}></Post.PostIcon>
+              </Post.FooterRoot>
+            </Post.PostSection>
+          </Post.Root>
+
+          <Post.Root>
+            <Post.Header />
+            <Post.PostSection>
+              <Post.SectionHeader />
+              <Post.Content texto="Pegue seu café, estou codando...👩‍💻💻"/>
+              <Post.PostImage src={capaDesenvolvimento} href={"" || ''}/>
+              <Post.FooterRoot>
+                <Post.PostIcon Icon={MessageCircle} onClick={()=> {
+                  toast.warn('Em breve, você poderá comentar nesse post')
+                }}>
+                  <Post.Amount amount={150} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={Heart} onClick={()=> {
+                  toast.warn('Em breve, você poderá curtir esse post')
+                }}>
+                  <Post.Amount amount={500} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={BarChart2} onClick={()=> {
+                  toast.warn('Em breve, total de visualizações')
+                }}>
+                  <Post.Amount amount={1000} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={Share2}></Post.PostIcon>
+              </Post.FooterRoot>
+            </Post.PostSection>
+          </Post.Root>
+          <Post.Root>
+            <Post.Header />
+            <Post.PostSection>
+              <Post.SectionHeader />
+              <Post.Content texto="Pegue seu café, estou codando...👩‍💻💻"/>
+              <Post.PostImage src={capaDesenvolvimento} href={"" || ''}/>
+              <Post.FooterRoot>
+                <Post.PostIcon Icon={MessageCircle} onClick={()=> {
+                  toast.warn('Em breve, você poderá comentar nesse post')
+                }}>
+                  <Post.Amount amount={150} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={Heart} onClick={()=> {
+                  toast.warn('Em breve, você poderá curtir esse post')
+                }}>
+                  <Post.Amount amount={500} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={BarChart2} onClick={()=> {
+                  toast.warn('Em breve, total de visualizações')
+                }}>
+                  <Post.Amount amount={1000} />
+                </Post.PostIcon>
+                <Post.PostIcon Icon={Share2}></Post.PostIcon>
+              </Post.FooterRoot>
+            </Post.PostSection>
+          </Post.Root>
+          <Post.Root>
+            <Post.Header />
+            <Post.PostSection>
+              <Post.SectionHeader />
+              <Post.Content texto="Pegue seu café, estou codando...👩‍💻💻"/>
+              <Post.PostImage src={capaDesenvolvimento} href={"" || ''}/>
               <Post.FooterRoot>
                 <Post.PostIcon Icon={MessageCircle} onClick={()=> {
                   toast.warn('Em breve, você poderá comentar nesse post')
