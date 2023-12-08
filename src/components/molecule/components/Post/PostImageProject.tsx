a7aaf8f37c1614cd691b4c4fa50e95b75aa05b42
Diegoh-dev <diegoh.developer@gmail.com>
2023-12-07 23:06:19 -0300

    feat: colocando component de link apenas se tiver url.

--- src/components/molecule/components/Post/PostImageProject.tsx
+++ src/components/molecule/components/Post/PostImageProject.tsx
@@ -10,17 +10,30 @@ export function PostImageProject({ src,href }: PostImageProjectProps) {
   console.log({href})
   return (
     <section className="relative  max-h-[26rem] mt-2 min-h-fit h-[26rem] overflow-hidden rounded-2xl">
-      <Link href={href ? href : ''} target="_blank">
+      {href ? (
+         <Link href={href ? href : ''} target="_blank">
+         <Image
+           src={src}
+           // width={100}
+           // height={100}
+           alt="capa projeto"
+           loading="lazy"
+           fill={true}
+           objectFit="cover"
+         />
+       </Link>
+      ) : (
         <Image
-          src={src}
-          // width={100}
-          // height={100}
-          alt="capa projeto"
-          loading="lazy"
-          fill={true}
-          objectFit="cover"
-        />
-      </Link>
+        src={src}
+        // width={100}
+        // height={100}
+        alt="capa projeto"
+        loading="lazy"
+        fill={true}
+        objectFit="cover"
+      />
+      )}
+     
     </section>
   );
 }
